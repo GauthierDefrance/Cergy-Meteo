f81c00b8d404efffdf10b326b315da2814aa6427
ThomasHGit <162038062+ThomasHGit@users.noreply.github.com>
2025-03-20 21:24:57 +0100

    Update grab.inc.php

--- src/include/grab.inc.php
+++ src/include/grab.inc.php
@@ -1,7 +1,11 @@
 <?php
-    $imageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1024px-Wikipedia-logo-v2.svg.png";
-    $path = "ressources/image_du_jour.png"; 
+    $imageUrl = "https://fastly.picsum.photos/id/88/600/400.jpg?hmac=XiB6m5M3RBq7_L0vHoWJN9GCEO_2V_UhQ2IYesftCbg";
+    $path = "../ressources/image_du_jour.png"; 
     $image = file_get_contents($imageUrl);
+    if ($image === false) {
+        echo "Failed to download image.<br>";
+        print_r(error_get_last());
+    }
     file_put_contents($path, $image);
 
 ?>
