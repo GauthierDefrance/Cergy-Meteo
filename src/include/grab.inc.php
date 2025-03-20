9826d0d3547ead6958791377760208b7cb908103
ThomasHGit <162038062+ThomasHGit@users.noreply.github.com>
2025-03-20 20:46:03 +0100

    Update grab.inc.php

--- src/include/grab.inc.php
+++ src/include/grab.inc.php
@@ -1,5 +1,7 @@
 <?php
     $imageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1024px-Wikipedia-logo-v2.svg.png";
-    $path = "ressources/"; 
+    $path = "ressources/image_du_jour.png"; 
+    $image = file_get_contents($imageUrl);
+    file_put_contents($path, $image);
 
 ?>
