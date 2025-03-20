08f91c5e16fb041c98e769a9a6957dd810bb47c8
GauthierDefrance <defrance.gauthier@gmail.com>
2025-03-20 20:48:40 +0100

    aaa

---
+++ src/error.php
@@ -0,0 +1,15 @@
+<?php
+require "./include/header.inc.php";
+?>
+
+<main>
+    <h1>Oups !</h1>
+    <section>
+        <p>Il semblerait qu'il y a eu une erreur lors de la connexion !</p>
+    </section>
+
+</main>
+
+<?php
+require "./include/footer.inc.php";
+?>
