aa51dad34a4e2711a49b7a477dea6740958bcaf6
ThomasHGit <162038062+ThomasHGit@users.noreply.github.com>
2025-03-20 22:17:05 +0100

    script to grab image and JSON from nasa

--- src/include/grab.inc.php
+++ src/include/grab.inc.php
@@ -1,11 +1,27 @@
 <?php
-    $imageUrl = "https://fastly.picsum.photos/id/88/600/400.jpg?hmac=XiB6m5M3RBq7_L0vHoWJN9GCEO_2V_UhQ2IYesftCbg";
-    $path = "../ressources/image_du_jour.png"; 
+    require_once('../protected/KEYS.php');
+
+    $jsonUrl = "https://api.nasa.gov/planetary/apod?api_key=".$NASA_KEY;
+    $jsonPath= '../ressources/nasa_file.JSON';
+    $json = file_get_contents($jsonUrl);
+    if ($json === false) {
+        echo "❌ Failed to fetch JSON.";
+    }
+    else{
+        echo $json;
+        file_put_contents($jsonPath, $json);
+    }
+
+    $data = json_decode($json, true);
+
+
+    $imageUrl = $data['url'];
+    $imgPath = "../ressources/image_du_jour.png"; 
     $image = file_get_contents($imageUrl);
     if ($image === false) {
         echo "Failed to download image.<br>";
         print_r(error_get_last());
     }
-    file_put_contents($path, $image);
-
+    file_put_contents($imgPath, $image);
+    
 ?>
