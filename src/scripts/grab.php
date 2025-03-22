e03442d3ac453923fe2db4fef03f805d0d5ab09a
ThomasHGit <162038062+ThomasHGit@users.noreply.github.com>
2025-03-23 00:22:24 +0100

    fix grab png bug

--- src/scripts/grab.php
+++ src/scripts/grab.php
@@ -20,13 +20,14 @@
 
     if($data['media_type']=="image"){
         echo "hey";
-        $imgPath .= "image_du_jour.png";
+        $imgPath .= "image_du_jour.jpg";
         $image = file_get_contents($imageUrl);
         if ($image === false) {
             echo "Failed";
             print_r(error_get_last());
         }
         file_put_contents($imgPath, $image);
+        echo "put content in";
     }
     elseif($data['media_type']=="video"){
         if (strpos($data['url'], '.mp4') !== false) {
